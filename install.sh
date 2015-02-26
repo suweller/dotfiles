7ab9b88dc4e1722514bf43a496eaf5dc26e8e6c7
Steven Weller <github@suweller.com>
2015-02-26 14:27:16 +0100

    Install xcode and brew

--- install.sh
+++ install.sh
@@ -1,25 +1,57 @@
 #!/bin/bash
-cd lib
-function yesno() {
-  read -p "Force symlink for .$1? (y/n)" -n 1 -r
+
+function yes-no() {
+  read -p "$1 [y/n]" -n 1 -r
   echo '' # make sure we're on a newline
   [ $REPLY == "y" ] && return 0 || return 1
 }
-for entry in *; do
-  yesno $entry || continue
-  ln -Ffs $PWD/$entry $HOME/.$entry
-done
 
-vundle="$PWD/vim/bundle/vundle.vim"
+if [[ ! $(xcode-select -p) ]]; then
+  $(xcode-select --install)
+  yes-no 'Did Xcode install successfully?' || exit 1
+fi
+
+if [[ ! -x /usr/local/bin/brew ]]; then
+  echo "Install homebrew"
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+function brew-all {
+  function may-brew {
+    printf "%-50s" $1
+    if brew ${assert} -1 | grep -q $(echo $1 | sed 's/caskroom\/cask\///'); then
+      printf "%30s\n" '[OK]'
+    else
+      printf "%30s\n\n" '[INSTALLING]'
+      brew ${action} $1
+      printf "\n\n"
+    fi
+    return 0
+  }
+
+  function many-brew {
+    printf "\nbrew $2\n\n"
+    local assert=$1
+    local action=$2
+    for item in ${brews[@]}; do
+      may-brew $item
+    done
+  }
+
+} && brew-all
+
+cd lib
+for entry in *; do
+  ln -Ffs .dotfiles/lib/$entry $HOME/.$entry
+done
+cd ..
+
+vundle="$PWD/lib/vim/bundle/vundle.vim"
 [ -a $vundle ] || (git clone git@github.com:gmarik/Vundle.vim.git $vundle && \
   vim +PluginInstall +qall) # Fetch and install all bundles included in vimrc
-
-cd ..
 
 oh_my_zsh="$PWD/oh-my-zsh"
 [ -a $oh_my_zsh ] || git clone git@github.com:robbyrussell/oh-my-zsh.git $oh_my_zsh
 
-yesno 'id_rsa.pub' && ln -Ffs $PWD/id_rsa.pub $HOME/.ssh/.id_rsa.pub
-
-echo "You can make zsh your default shell by running:"
-echo "chsh -s /bin/zsh"
+ln -Ffs .dotfiles/id_rsa.pub $HOME/.ssh/.id_rsa.pub
+chsh -s "$(which zsh)"

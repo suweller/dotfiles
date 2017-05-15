d4a90142a95140ba93ab5b548aa4fd6b3aedb585
Steven Weller <github@suweller.com>
2017-05-15 22:38:32 +0200

    Improve battery script;and its output

--- bin/battery.sh
+++ bin/battery.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
-TYPE=`pmset -g batt | grep 'drawing' | awk '{print $4}' | awk -F "'" '{print $2}'`
-LEVEL=`pmset -g batt | grep Internal | awk '{print $3}' | awk -F '%' '{print $1}'`
-echo "$LEVEL% - $TYPE"
+if [ `uname` = 'Darwin' ] ; then
+  percent=`ioreg -l \
+    | grep -i capacity \
+    | tr '\n' ' | ' \
+    | awk '{printf("%d", $10/$5 * 100)}'`
+fi
+
+direction='#[nobright fg=red]-'
+if [[ `pmset -g batt | grep 'Now drawing from'` == *"AC Power"* ]]; then
+  direction='#[nobright fg=green]+'
+fi
+
+if (( percent > 74)); then color='#[fg=green]'
+elif (( percent > 49 )); then color='#[fg=yellow]'
+elif (( percent > 24 )); then color='#[fg=orange]'
+else color='#[bright fg=red]'
+fi
+echo "$color$percent%$direction"

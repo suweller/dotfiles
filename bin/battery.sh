4e03ae30596d909a567ff9bba528caaa06703a64
Steven Weller <github@suweller.com>
2017-12-01 11:09:41 +0100

    Display battery level as vulgar fraction

--- bin/battery.sh
+++ bin/battery.sh
@@ -6,9 +6,9 @@ if [ `uname` = 'Darwin' ] ; then
     | awk '{printf("%d", $10/$5 * 100)}'`
 fi
 
-direction='#[nobright fg=red]-'
+direction='#[nobright]'
 if [[ `pmset -g batt | grep 'Now drawing from'` == *"AC Power"* ]]; then
-  direction='#[nobright fg=green]+'
+  direction='#[bright]'
 fi
 
 if (( percent > 74)); then color='#[fg=green]'
@@ -16,4 +16,16 @@ elif (( percent > 49 )); then color='#[fg=yellow]'
 elif (( percent > 24 )); then color='#[fg=orange]'
 else color='#[bright fg=red]'
 fi
-echo "$color$percent%$direction"
+
+if ((percent > 97)); then icon='⅟'
+elif ((percent > 87)); then icon='⅞'
+elif ((percent > 75)); then icon='¾'
+elif ((percent > 62)); then icon='⅝'
+elif ((percent > 50)); then icon='½'
+elif ((percent > 37)); then icon='⅜'
+elif ((percent > 25)); then icon='¼'
+elif ((percent > 12)); then icon='⅛'
+else icon='0'
+fi
+
+echo "$color$direction$icon#[nobright]"

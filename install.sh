58a41da6e41591c592b455ad19e43c1fb14132f3
Steven Weller <github@suweller.com>
2022-07-08 16:08:03 +0200

    Update

--- install.sh
+++ install.sh
@@ -16,6 +16,10 @@ if [[ ! -x /usr/local/bin/brew ]]; then
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
+if [[ ! -x  ~/.tmux/plugins/tpm ]]; then
+  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+fi
+
 function brew-all {
   function may-brew {
     printf "%-50s" $1
